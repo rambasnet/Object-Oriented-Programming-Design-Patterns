5d8948130914f26dd5cc1e417b0559da164099fd
Ram Basnet <rambasnet@gmail.com>
2024-10-24 16:13:02 +0000

    fix 5

--- notebooks/src/unittesting/inventory/test_order_fixed.py
+++ notebooks/src/unittesting/inventory/test_order_fixed.py
@@ -17,7 +17,7 @@ class TestOrder(unittest.TestCase):
         self.wh = Warehouse({'shoes': 10, 'hats': 2, 'umbrellas': 0})
 
     @given(item=st.sampled_from(['shoes', 'hats', 'umbrellas']),
-           quantity=st.integers(min_value=1, max_value=4))
+           quantity=st.integers(min_value=1, max_value=100))
     def test_stock_level_plus_quantity_equals_initial_stock_level(
             self,
             item: str,
